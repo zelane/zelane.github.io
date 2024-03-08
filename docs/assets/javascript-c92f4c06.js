806484f4b445e7fcb530b4bbb3238215f9514629
Zelane <zelane@hollow.tech>
2024-03-08 19:41:03 +0000

    update and build

--- docs/assets/javascript-c92f4c06.js
+++ docs/assets/javascript-c92f4c06.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-c550f86f.js";import"./index-cb77cbec.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-8d635648.js";import"./index-89ffd41a.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.44.0(3e047efd345ff102c8c61b5398fb30845aaac166)
  * Released under the MIT license
--- docs/assets/razor-72eca567.js
+++ docs/assets/razor-72eca567.js
@@ -1,4 +1,4 @@
-import{m as s}from"./index-cb77cbec.js";/*!-----------------------------------------------------------------------------
+import{m as s}from"./index-89ffd41a.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.44.0(3e047efd345ff102c8c61b5398fb30845aaac166)
  * Released under the MIT license
